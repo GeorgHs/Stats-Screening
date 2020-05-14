aa7a35c6c10dcfa008b2dfe45c00139b709e4d28
GeorgHs <50588349+GeorgHs@users.noreply.github.com>
2020-05-14 09:55:44 +0200

    1 view test

--- Django_Project/PortfolioScreen/Tests/test_views.py
+++ Django_Project/PortfolioScreen/Tests/test_views.py
@@ -4,8 +4,9 @@ from django.urls import reverse
 
 class TestViews(TestCase):
 
-    def test_project_list_GET(self):
+
+    def test_project_about_GET(self):
         client = Client()
-        response = self.client.get(reverse('list'))
+        response = self.client.get(reverse('about'))
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'budget/project-list.html')
+        self.assertTemplateUsed(response, 'PortfolioScreen/about.html')
